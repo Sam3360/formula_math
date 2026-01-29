ea43a57c406a6319fa61ef9ac95dfc56ce0cf09d
Sam3360 <iforgot3360@gmail.com>
2026-01-29 16:52:27 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="formula_math",
+    version="1.0.0",
+    packages=find_packages(),
+    description="All-in-one math, physics, finance, geometry formula library for Python",
+    author="Samarth Chugh",
+    author_email="iforgot3360@gmail.com",
+    url="https://github.com/Sam3360/formula_math",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
